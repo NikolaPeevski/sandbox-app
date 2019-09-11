b727bc2b6234fa16914ae1ae0264ccf9c8daa6f0
npeevski <n.peevski@billiton.de>
2019-09-11 17:25:28 +0300

    Init commit

---
+++ src/app/time-picker/time-picker.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TimepickerComponent } from './timepicker/timepicker.component';
+
+
+
+@NgModule({
+  declarations: [TimepickerComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TimePickerModule { }
